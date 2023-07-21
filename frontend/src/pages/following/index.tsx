1a0649f58335672efbb2de0548d29fa288959017
lazarobodevan <lazarobodevan@gmail.com>
2023-07-21 18:06:53 -0300

    feat(front): página de seguidores

--- frontend/src/pages/following/index.tsx
+++ frontend/src/pages/following/index.tsx
@@ -1,9 +1,12 @@
+import RecipeCard from '../../components/recipe-card'
+import SearchComponent from '../search/search-component'
 import styles from './Following.module.scss'
 
 export default function Following(){
     return(
-        <section>
-            Following
+        <section className={styles.content}>
+            <SearchComponent/>
+            <RecipeCard/>
         </section>
     )
 }
